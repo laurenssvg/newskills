51d3a890715b5dfbb340fe9fbafe47940149dd5c
laurenssvg <lvangellecum@gmail.com>
2021-06-09 01:18:02 +0200

    Conclusie toegevoegd

--- pages/api/info.js
+++ pages/api/info.js
@@ -4,27 +4,33 @@ export default (req, res) => {
   res.status(200).json([
     {
       id: 1,
-      name: "Introductie",
+      name: "1. Introductie",
       image:
         "https://blue-c-lms.s3.eu-central-1.amazonaws.com/content/Introductie-cursus.png",
     },
     {
       id: 2,
-      name: "Kompas",
+      name: "2. Kompas",
       image:
         "https://deinterieurstickerzaak.nl/wp-content/uploads/2018/06/Kompas-zwart.png",
     },
     {
       id: 3,
-      name: "Doelen",
+      name: "3. Doelen",
       image:
         "https://dyllifedesign.nl/wp-content/uploads/2018/12/Goals-step-by-step.jpg",
     },
     {
       id: 4,
-      name: "Resultaten",
+      name: "4. Resultaten",
       image:
         "https://lh3.googleusercontent.com/proxy/XjZsnGu8IxJ56fxRPqBp5FlLD4Gw0WOeCl9fcFGmlgZud_fBynE9bmsEnZXFWmQWT6eYtE9VNdRP6qMrHBucDJjARkEs2DExP_ArZizJeAp5QBwudXo7TpSYD3j3ZpW6OTJm-vTe5g",
+    },
+    {
+      id: 5,
+      name: "5. Conclusie",
+      image:
+        "http://www.educatie21.nl/wp-content/uploads/2017/05/conclusion.jpg",
     },
   ]);
 };
